546b9276c1026afe4939587bf202e8d3eca3a561
Hack Reactor Students <accounts@hackreactor.com>
2015-05-20 17:26:31 -0700

    complete pseudo-classical style dancer and blinkydancer classes

--- src/dancer.js
+++ src/dancer.js
@@ -1,4 +1,4 @@
-// delete this later, keep for comparison
+// // delete this later, keep for comparison
 // // Creates and returns a new dancer object that can step
 // var makeDancer = function(top, left, timeBetweenSteps){
 
@@ -31,22 +31,24 @@
 //   dancer.setPosition(top, left);
 
 //   return dancer;
-// // };
+// };
 
-// =====================================
+// // =====================================
 
 var makeDancer = function(top, left, timeBetweenSteps){
   this.$node = $('<span class="dancer"></span>');
+  // this.timeBetweenSteps = timeBetweenSteps;
+  // this.top = top;
+  // this.left = left;
   this.step();
   this.setPosition(top, left);
 };
 
-makeDancer.prototype.step = function(){
-   // the basic dancer doesn't do anything interesting at all on each step,
-  // it just schedules the next step
-  setTimeout(this.step, this.timeBetweenSteps);
-};
 
+makeDancer.prototype.step = function () {
+  setTimeout(this.step.bind(this), this.timeBetweenSteps);
+  console.dir(this);
+}
 makeDancer.prototype.setPosition = function(top, left){
   // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
@@ -57,3 +59,17 @@ makeDancer.prototype.setPosition = function(top, left){
     };
     this.$node.css(styleSettings);
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
